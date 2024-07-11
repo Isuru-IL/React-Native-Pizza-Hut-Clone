056858a02ec55a6e14cdeb5a1013a70eb41fee76
isuru <isurulakshan607@gmail.com>
2024-07-11 22:25:06 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import LogIn from "@/components/pages/LogIn";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Text>Edit app/index.tsx to edit this screen.</Text>
+    // </View>
+    <LogIn/>
+  );
+}
